5134158aab664889fdd929c2205b5e4549bc07ae
Duy Thien <duythien201204@gmail.com>
2023-12-27 20:13:38 +0700

    them man hinh chon quiz va lam quiz

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import 'package:new_project/models/question.dart';
+
+List<Question> questions = [
+  Question('q1', 'Choose A', ['A', 'B', 'C', 'D'], 0),
+  Question('q2', 'Choose B', ['A', 'B', 'C', 'D'], 1),
+  Question('q3', 'Choose C', ['A', 'B', 'C', 'D'], 2),
+  Question('q4', 'Choose D', ['A', 'B', 'C', 'D'], 3),
+  Question('q5', 'Choose yes', ['yes', 'no'], 0),
+  Question('q6', 'Choose no', ['yes', 'no'], 1),
+];
---
+++ lib/selectClassScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:new_project/data/classes.dart';
+import 'package:new_project/quizListScreen.dart';
+import 'package:new_project/utils/app_styles.dart';
+
+class SelectClassScreen extends StatelessWidget {
+  const SelectClassScreen({super.key, required this.setScreen});
+  final void Function(Widget screen) setScreen;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: [
+          for (var lop in classes)
+            TextButton(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      lop.classID + ' - ' + lop.className,
+                      style: const TextStyle(
+                          fontSize: 20, color: AppThemes.headingColor),
+                    ),
+                    Text('Teacher: ${lop.teacherID}'),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                setScreen(QuizListScreen(
+                  classID: lop.classID,
+                  className: lop.className,
+                  setScreen: setScreen,
+                ));
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
