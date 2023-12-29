a3436de3be347f296853ffca3ee0831754b981c4
Duy Thien <duythien201204@gmail.com>
2023-12-29 21:43:47 +0700

    edit addQuizScreen

--- lib/screens/quistList/addQuizScreen.dart
+++ lib/screens/quistList/addQuizScreen.dart
@@ -11,117 +11,89 @@ class AddQuizScreen extends StatefulWidget {
 }
 
 class _AddQuizScreenState extends State<AddQuizScreen> {
-  DateTime startTime = DateTime.now(), endTime = DateTime.now();
-  final TextEditingController lengthController = TextEditingController();
-  final TextEditingController weightController = TextEditingController();
-  final TextEditingController notificationController = TextEditingController();
-  final TextEditingController quizNameController = TextEditingController();
+  int nQuestion = 0;
+  String quizName = '';
+  int length = 0;
+  double weight = 0;
+  List<String> questions = [];
+  List<List<String>> answers = [];
+  List<int> correctAnswers = [];
 
-  @override
-  void dispose() {
-    lengthController.dispose();
-    weightController.dispose();
-    notificationController.dispose();
-    quizNameController.dispose();
-    super.dispose();
-  }
-
-  void _selectStartTime() async {
+  List<DateTime?> dates = [DateTime.now(), DateTime.now()];
+  void _presentDatePicker(i) async {
     final now = DateTime.now();
-    final firstDate = DateTime.now();
+    final firstDate = now;
     final lastDate = DateTime(now.year + 1, now.month, now.day);
-    final pickedDate = await showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: lastDate);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: lastDate);
 
     setState(() {
-      startTime = pickedDate!;
+      dates[i] = pickedDate;
     });
   }
 
-  void _selectEndTime() async {
-    final now = DateTime.now();
-    final firstDate = DateTime.now();
-    final lastDate = DateTime(now.year + 1, now.month, now.day);
-    final pickedDate = await showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: lastDate);
-
-    setState(() {
-      endTime = pickedDate!;
-    });
+  void setQuestion(index, input) {
+    questions[index] = input;
   }
 
-  List<int> questionID = [];
+  void setAnswer(questionID, answerID, input) {
+    answers[questionID][answerID] = input;
+  }
+
+  void setCorrectAnswer(questionID, input) {
+    correctAnswers[questionID] = int.parse(input) - 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
+    while (questions.length < nQuestion) {
+      questions.add('');
+      answers.add(['', '', '', '']);
+      correctAnswers.add(-1);
+    }
+    while (questions.length > nQuestion) {
+      questions.removeLast();
+      answers.removeLast();
+      correctAnswers.removeLast();
+    }
     return SingleChildScrollView(
       child: Container(
-        padding: const EdgeInsets.all(100),
-        width: screenWidth,
-        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              'Create quiz',
+              'Create question',
               style: Theme.of(context).textTheme.titleLarge,
             ),
-            const SizedBox(height: 20),
-            customTextField(screenWidth: screenWidth * 5, screenHeight: screenHeight, text: 'Enter quiz name', controller: quizNameController),
-            _SelectTime(
-              text: 'Start time',
-              time: startTime,
-              selectTime: _selectStartTime,
-            ),
-            _SelectTime(
-              text: 'End time',
-              time: endTime,
-              selectTime: _selectEndTime,
-            ),
-            customTextField(controller: lengthController, text: "Enter lenght", screenWidth: screenWidth * 5, screenHeight: screenHeight),
-            customTextField(controller: weightController, text: "Enter weight", screenWidth: screenWidth * 5, screenHeight: screenHeight),
-            customTextField(controller: notificationController, text: "Enter notificaton", screenWidth: screenWidth * 5, screenHeight: screenHeight),
-            for (var i in questionID)
-              Row(
-                children: [
-                  CreateQuestion(key: Key(i.toString())),
-                  IconButton.filledTonal(
-                    onPressed: () {
-                      setState(() {
-                        questionID.remove(i);
-                      });
-                    },
-                    icon: const Icon(Icons.remove),
-                  )
-                ],
-              ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  questionID.add(questionID.length);
-                });
-              },
-              child: const Text('Add question'),
-            ),
-            customTextField(screenWidth: screenWidth * 5, screenHeight: screenHeight, text: 'Enter length', controller: lengthController),
-            customTextField(screenWidth: screenWidth * 5, screenHeight: screenHeight, text: 'Enter weight', controller: weightController),
-            customTextField(screenWidth: screenWidth * 5, screenHeight: screenHeight, text: 'Enter notification', controller: notificationController),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Cancel'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Add Quiz'),
-                ),
-              ],
-            )
+            TextInput(
+                name: 'Quiz name',
+                onChange: (input) {
+                  quizName = input;
+                }).createTextField(),
+            TextInput(
+                name: 'Number of question (number)',
+                onChange: (input) {
+                  setState(() {
+                    nQuestion = int.parse(input);
+                  });
+                }).createTextField(),
+            TextInput(
+                name: 'Length (minutes)',
+                onChange: (input) {
+                  length = int.parse(input);
+                }).createTextField(),
+            TextInput(
+                name: 'Weight',
+                onChange: (input) {
+                  weight = double.parse(input);
+                }).createTextField(),
+            for (var i = 0; i < nQuestion; i++)
+              createQuestion(i, setQuestion, setAnswer, setCorrectAnswer),
           ],
         ),
       ),
@@ -129,74 +101,58 @@ class _AddQuizScreenState extends State<AddQuizScreen> {
   }
 }
 
-class CreateQuestion extends StatefulWidget {
-  const CreateQuestion({super.key});
-
-  @override
-  State<CreateQuestion> createState() => _CreateQuestionState();
+class TextInput {
+  TextInput({required this.name, required this.onChange});
+  String name;
+  final void Function(String input) onChange;
+  TextField createTextField() {
+    return TextField(
+      onChanged: onChange,
+      decoration: InputDecoration(
+        label: Text(name),
+      ),
+    );
+  }
 }
 
-class _CreateQuestionState extends State<CreateQuestion> {
-  final TextEditingController questionController = TextEditingController();
-  final TextEditingController answer1Controller = TextEditingController();
-  final TextEditingController answer2Controller = TextEditingController();
-  final TextEditingController answer3Controller = TextEditingController();
-  final TextEditingController answer4Controller = TextEditingController();
-
-  @override
-  void dispose() {
-    questionController.dispose();
-    answer1Controller.dispose();
-    answer2Controller.dispose();
-    answer3Controller.dispose();
-    answer4Controller.dispose();
-    return super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
-
-    return Card(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
-        ),
-      ),
+Widget createQuestion(index, setQuestion, setAnswer, setCorrectAnswer) {
+  return Card(
+    child: Container(
+      padding: const EdgeInsets.all(20),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          customTextField(controller: questionController, text: 'Enter question', screenWidth: screenWidth, screenHeight: screenHeight),
-          customTextField(controller: answer1Controller, text: 'Enter question', screenWidth: screenWidth, screenHeight: screenHeight),
-          customTextField(controller: answer2Controller, text: 'Enter question', screenWidth: screenWidth, screenHeight: screenHeight),
-          customTextField(controller: answer3Controller, text: 'Enter question', screenWidth: screenWidth, screenHeight: screenHeight),
-          customTextField(controller: answer4Controller, text: 'Enter question', screenWidth: screenWidth, screenHeight: screenHeight),
+          Text(
+            'Question ${index + 1}',
+            style: const TextStyle(color: Colors.black54, fontSize: 16),
+          ),
+          TextField(
+            onChanged: (input) {
+              setQuestion(index, input);
+            },
+            decoration: const InputDecoration(
+              label: Text('Question'),
+            ),
+          ),
+          for (var answerID = 0; answerID < 4; answerID++)
+            TextField(
+              onChanged: (input) {
+                setAnswer(index, answerID, input);
+              },
+              decoration: const InputDecoration(
+                label: Text('Answer'),
+              ),
+            ),
+          TextField(
+            onChanged: (input) {
+              setCorrectAnswer(index, input);
+            },
+            decoration: const InputDecoration(
+              label: Text('Correct answer (1-4)'),
+            ),
+          )
         ],
       ),
-    );
-  }
-}
-
-class _SelectTime extends StatelessWidget {
-  const _SelectTime({super.key, required this.text, required this.time, required this.selectTime});
-  final void Function() selectTime;
-  final DateTime time;
-  final String text;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        IconButton(
-          onPressed: () {
-            selectTime();
-          },
-          icon: const Icon(Icons.calendar_month),
-        ),
-        Text(
-          '$text: ${time.day}/${time.month}/${time.year}',
-        ),
-      ],
-    );
-  }
+    ),
+  );
 }
